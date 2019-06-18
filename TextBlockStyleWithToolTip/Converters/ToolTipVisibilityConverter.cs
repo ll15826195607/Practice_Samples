d87bd1874fccd564ad5ea8a0c340d2839a757f1a
ll15826195607 <luli15826195607@gmail.com>
2019-06-18 11:48:55 +0800

    新建TextBlockStyleWithToolTip项目

---
+++ TextBlockStyleWithToolTip/Converters/ToolTipVisibilityConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace TextBlockStyleWithToolTip.Converter
+{
+    public class ToolTipVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return Visibility.Collapsed;
+            }
+            else
+            {
+                FrameworkElement textBlock = (FrameworkElement)value;
+                textBlock.Measure(new Size(Double.PositiveInfinity, Double.PositiveInfinity));
+                var width = textBlock.DesiredSize.Width;
+                if (((FrameworkElement)value).ActualWidth < width)
+                {
+                    return Visibility.Visible;
+                }
+                else
+                {
+                    return Visibility.Collapsed;
+                }
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
