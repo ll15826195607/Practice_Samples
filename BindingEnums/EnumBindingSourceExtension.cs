961179626cc6a9789ccffc0f75fcf8def2149b04
ll15826195607 <luli15826195607@gmail.com>
2020-01-14 15:32:27 +0800

    更改EnumBindingSourceExtension

--- BindingEnums/EnumBindingSourceExtension.cs
+++ BindingEnums/EnumBindingSourceExtension.cs
@@ -39,13 +39,17 @@ namespace BindingEnums
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
             if (null == this._enumType)
+            {
                 throw new InvalidOperationException("The EnumType must be specified.");
+            }
 
             Type actualEnumType = Nullable.GetUnderlyingType(this._enumType) ?? this._enumType;
             Array enumValues = Enum.GetValues(actualEnumType);
 
             if (actualEnumType == this._enumType)
+            {
                 return enumValues;
+            }
 
             Array tempArray = Array.CreateInstance(actualEnumType, enumValues.Length + 1);
             enumValues.CopyTo(tempArray, 1);
