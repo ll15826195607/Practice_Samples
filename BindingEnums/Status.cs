f1429f5b28b395d9a25a045cb577f55f4df22acf
ll15826195607 <luli15826195607@gmail.com>
2020-01-14 15:25:28 +0800

    Enum作为绑定Source

---
+++ BindingEnums/Status.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace BindingEnums
+{
+    public enum Status
+    {
+        [Description("This is horrible")]
+        Horrible,
+        [Description("This is bad")]
+        Bad,
+        [Description("This is so so")]
+        SoSo,
+        [Description("This is Good")]
+        Good,
+        [Description("This is Better")]
+        Better,
+        [Description("This is Best")]
+        Best
+    }
+}
