7903520e43666769499ff41128c7834ec4b1e400
luli <luli@DESKTOP-5PCEHQU>
2020-03-19 17:02:25 +0800

    添加自定义NumbericUpDown控件

---
+++ ToolKit.Wccl/NumericUpDown.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToolKit.Wccl
+{
+    /// <summary>
+    /// 按照步骤 1a 或 1b 操作，然后执行步骤 2 以在 XAML 文件中使用此自定义控件。
+    ///
+    /// 步骤 1a) 在当前项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ToolKit.Wccl"
+    ///
+    ///
+    /// 步骤 1b) 在其他项目中存在的 XAML 文件中使用该自定义控件。
+    /// 将此 XmlNamespace 特性添加到要使用该特性的标记文件的根
+    /// 元素中:
+    ///
+    ///     xmlns:MyNamespace="clr-namespace:ToolKit.Wccl;assembly=ToolKit.Wccl"
+    ///
+    /// 您还需要添加一个从 XAML 文件所在的项目到此项目的项目引用，
+    /// 并重新生成以避免编译错误:
+    ///
+    ///     在解决方案资源管理器中右击目标项目，然后依次单击
+    ///     “添加引用”->“项目”->[选择此项目]
+    ///
+    ///
+    /// 步骤 2)
+    /// 继续操作并在 XAML 文件中使用控件。
+    ///
+    ///     <MyNamespace:NumericUpDown/>
+    ///
+    /// </summary>
+    [TemplatePart(Name = "UpButtonElement", Type = typeof(RepeatButton))]
+    [TemplatePart(Name = "DownButtonElement", Type = typeof(RepeatButton))]
+    [TemplateVisualState(Name = "Positive", GroupName = "ValueStates")]
+    [TemplateVisualState(Name = "Negative", GroupName = "ValueStates")]
+    [TemplateVisualState(Name = "Focused", GroupName = "FocusedStates")]
+    [TemplateVisualState(Name = "Unfocused", GroupName = "FocusedStates")]
+    public class NumericUpDown : Control
+    {
+        public NumericUpDown()
+        {
+            DefaultStyleKey = typeof(NumericUpDown);
+            this.IsTabStop = true;
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(int), typeof(NumericUpDown),
+                new PropertyMetadata(new PropertyChangedCallback(ValueChangedCallback)));
+
+        public int Value
+        {
+            get
+            {
+                return (int)GetValue(ValueProperty);
+            }
+            set
+            {
+                SetValue(ValueProperty, value);
+            }
+        }
+
+        private static void ValueChangedCallback(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            NumericUpDown ctl = (NumericUpDown)obj;
+            int newValue = (int)args.NewValue;
+
+            // Call UpdateStates because the Value might have caused the
+            // control to change ValueStates.
+            ctl.UpdateStates(true);
+
+            // Call OnValueChanged to raise the ValueChanged event.
+            ctl.OnValueChanged(new ValueChangedEventArgs(NumericUpDown.ValueChangedEvent, newValue));
+        }
+
+        public static readonly RoutedEvent ValueChangedEvent = EventManager.RegisterRoutedEvent("ValueChanged", RoutingStrategy.Direct,
+                          typeof(ValueChangedEventHandler), typeof(NumericUpDown));
+
+        public event ValueChangedEventHandler ValueChanged
+        {
+            add { AddHandler(ValueChangedEvent, value); }
+            remove { RemoveHandler(ValueChangedEvent, value); }
+        }
+
+        protected virtual void OnValueChanged(ValueChangedEventArgs e)
+        {
+            // Raise the ValueChanged event so applications can be alerted when Value changes.
+            RaiseEvent(e);
+        }
+
+        private void UpdateStates(bool useTransitions)
+        {
+            if (Value >= 0)
+            {
+                VisualStateManager.GoToState(this, "Positive", useTransitions);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Negative", useTransitions);
+            }
+
+            if (IsFocused)
+            {
+                VisualStateManager.GoToState(this, "Focused", useTransitions);
+            }
+            else
+            {
+                VisualStateManager.GoToState(this, "Unfocused", useTransitions);
+            }
+        }
+
+        public override void OnApplyTemplate()
+        {
+            UpButtonElement = GetTemplateChild("UpButton") as RepeatButton;
+            DownButtonElement = GetTemplateChild("DownButton") as RepeatButton;
+            UpdateStates(false);
+        }
+
+        private RepeatButton downButtonElement;
+
+        private RepeatButton DownButtonElement
+        {
+            get
+            {
+                return downButtonElement;
+            }
+            set
+            {
+                if (downButtonElement != null)
+                {
+                    downButtonElement.Click -= new RoutedEventHandler(downButtonElement_Click);
+                }
+                downButtonElement = value;
+
+                if (downButtonElement != null)
+                {
+                    downButtonElement.Click += new RoutedEventHandler(downButtonElement_Click);
+                }
+            }
+        }
+
+        private void downButtonElement_Click(object sender, RoutedEventArgs e)
+        {
+            Value--;
+        }
+
+        private RepeatButton upButtonElement;
+
+        private RepeatButton UpButtonElement
+        {
+            get
+            {
+                return upButtonElement;
+            }
+
+            set
+            {
+                if (upButtonElement != null)
+                {
+                    upButtonElement.Click -= new RoutedEventHandler(upButtonElement_Click);
+                }
+                upButtonElement = value;
+
+                if (upButtonElement != null)
+                {
+                    upButtonElement.Click += new RoutedEventHandler(upButtonElement_Click);
+                }
+            }
+        }
+
+        private void upButtonElement_Click(object sender, RoutedEventArgs e)
+        {
+            Value++;
+        }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            Focus();
+        }
+
+
+        protected override void OnGotFocus(RoutedEventArgs e)
+        {
+            base.OnGotFocus(e);
+            UpdateStates(true);
+        }
+
+        protected override void OnLostFocus(RoutedEventArgs e)
+        {
+            base.OnLostFocus(e);
+            UpdateStates(true);
+        }
+    }
+
+    public delegate void ValueChangedEventHandler(object sender, ValueChangedEventArgs e);
+
+    public class ValueChangedEventArgs : RoutedEventArgs
+    {
+        private int _value;
+
+        public ValueChangedEventArgs(RoutedEvent id, int num)
+        {
+            _value = num;
+            RoutedEvent = id;
+        }
+
+        public int Value
+        {
+            get { return _value; }
+        }
+    }
+}
