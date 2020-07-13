c8859e4482085d73e676b02489512e7c05a7b67f
ll15826195607 <luli15826195607@gmail.com>
2020-07-13 20:59:02 +0800

    Add Adaptor Design Pattern

---
+++ AdapterDesignPattern/Adaptor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterDesignPattern
+{
+    /// <summary>
+    /// 类适配器：基于继承
+    /// </summary>
+    public class Adaptor : Adaptee, ITarget
+    {
+        public void f1()
+        {
+            base.fa();
+        }
+
+        public void f2()
+        {
+            Console.WriteLine("f2");
+        }
+    }
+}
