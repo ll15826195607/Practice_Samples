f6fcb90418ad6a3dbf8002de7264734ce06403dd
ll15826195607 <luli15826195607@gmail.com>
2020-07-13 21:18:07 +0800

    Update Proxy Design Pattern

---
+++ ProxyDemo/MetricsCollector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProxyDemo
+{
+    public class MetricsCollector
+    {
+        public readonly List<RequestEntity> Requests = new List<RequestEntity>();
+        public void RecordRequest(RequestEntity re)
+        {
+            this.Requests.Add(re);
+        }
+    }
+}
