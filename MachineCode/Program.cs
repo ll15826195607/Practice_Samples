40b4b606d1f0d3fb5b71e49e58b05a325857229b
luli15826195607@gmail.com <luli15826195607@gmail.com>
2021-03-19 15:41:43 +0800

    机器码

---
+++ MachineCode/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MachineCodeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.MachineName);
+            Console.WriteLine(Environment.OSVersion);
+            Console.WriteLine(MachineCode.GetSystemId());
+            Console.WriteLine(MachineCode.GetMachineCodeString());
+            Console.WriteLine(MachineCode.GetSystemId());
+            Console.ReadLine();
+        }
+    }
+}
